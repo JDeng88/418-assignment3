bba1eed02f67ca6fe35b3b107e45333be1626657
JDeng88 <jdeng2019@gmail.com>
2022-10-04 09:35:50 -0400

    remap port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 
-const port = 80;
+const port = 8088;
 
 app.use(express.urlencoded({ extended: true }));
 //app.set('view engine', 'ejs')
